8900eb9854a27e6adf7e8077e76f26af48d81240
colzak <joel.lauret@gmail.com>
2014-07-21 22:14:32 +0200

    --

--- web/js/app/views/ContactView.js
+++ web/js/app/views/ContactView.js
@@ -7,13 +7,23 @@ App.module("UserModule", function(UserModule, App, Backbone, Marionette, $, _){
         },
         sendMessage: function(e) {
         	e.preventDefault();
+            NProgress.start();
         	var message = new Message({ message: $('#message-content').val() }, { recipientId: this.model.get('id') });
         	message.save({}, {
         		success: function(response, model) {
-        			console.log(model);
+                    $('#message-content').val('');
+                    $('.message-sign').removeClass().addClass('message-sign glyphicon glyphicon-ok-sign');
+                    $('.message-text').html('Votre message a bien été envoyé !')
+                    $('.clzk-flash-messages-container').removeClass().addClass('clzk-flash-messages-container alert alert-success');
+                    $('.clzk-flash-messages-container').fadeIn( 400 ).delay( 3000 ).fadeOut( 400 );
+                    NProgress.done();
         		},
         		error: function(response, model) {
-        			console.log(response);
+        			$('.message-sign').removeClass().addClass('message-sign glyphicon glyphicon-remove-sign');
+                    $('.message-text').html('Une erreur est survenue. Veuillez réesayer ultérieurement ou contacter le support')
+                    $('.clzk-flash-messages-container').removeClass().addClass('clzk-flash-messages-container alert alert-danger');
+                    $('.clzk-flash-messages-container').fadeIn( 400 ).delay( 3000 ).fadeOut( 400 );
+                    NProgress.done();
         		}
         	});
         },
--- web/js/app/views/SearchView.js
+++ web/js/app/views/SearchView.js
@@ -363,15 +363,25 @@ App.module("SearchModule", function(SearchModule, App, Backbone, Marionette, $, 
         },
         sendMessage: function(e) {
             e.preventDefault();
+            NProgress.start();
             var messageContent = $('#message-content').val();
             var message = new Message({ message: messageContent }, { recipientId: this.model.get('id') });
 
             message.save({}, {
                 success: function(response, model) {
-                    // flashbag
+                    $('#message-content').val('');
+                    $('.message-sign').removeClass().addClass('message-sign glyphicon glyphicon-ok-sign');
+                    $('.message-text').html('Votre message a bien été envoyé !')
+                    $('.clzk-flash-messages-container').removeClass().addClass('clzk-flash-messages-container alert alert-success');
+                    $('.clzk-flash-messages-container').fadeIn( 400 ).delay( 3000 ).fadeOut( 400 );
+                    NProgress.done();
                 },
                 error: function(response) {
-                    // flashbag
+                    $('.message-sign').removeClass().addClass('message-sign glyphicon glyphicon-remove-sign');
+                    $('.message-text').html('Une erreur est survenue. Veuillez réesayer ultérieurement ou contacter le support')
+                    $('.clzk-flash-messages-container').removeClass().addClass('clzk-flash-messages-container alert alert-danger');
+                    $('.clzk-flash-messages-container').fadeIn( 400 ).delay( 3000 ).fadeOut( 400 );
+                    NProgress.done();
                 }
             });
         },
